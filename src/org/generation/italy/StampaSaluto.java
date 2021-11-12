79e9d975e22b4c4c9c1110d88ef9b4ff262633c3
GBosetti <gianmarco.bosetti@gmail.com>
2021-11-12 12:01:56 +0100

    aggiunto file di stampa saluto

---
+++ src/org/generation/italy/StampaSaluto.java
@@ -0,0 +1,11 @@
+package org.generation.italy;
+
+public class StampaSaluto {
+	// metodo eseguito dalla classe
+	public static void main(String[] args) {
+		// questa espressione stampa in console
+		System.out.println("Ciao gente di Italy Generation");
+		
+	}
+
+}
